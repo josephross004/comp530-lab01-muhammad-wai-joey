6c6b3c5b3a10fbc16fc9b2900979a7fbc763cced
Joseph Ross <ross802@proton.me>
2026-02-12 00:38:22 -0500

    efficiency

--- testharness.sh
+++ testharness.sh
@@ -25,13 +25,16 @@ for ((i = 1; i < N+1; i++)); do
 	# run the test under Valgrind
 
 	
-	# Valgrind
+	# Valgrind outline
 	#     valgrind must detect memory leaks, invalid read/write, use-after-free errors, double frees, 
 	#         unfreed LL nodes, unfreed strings in request_struct, and leaks from repeated test cycles
 	#     For each test case, confirm exit status is correct (that valgrind errors trigger a FAIL) and no 
 	#         leaks or invalid memory or uninit'd reads.
 	#     Entire test suite has to run under Valgrind automatically. 
 
+
+    # from valgrind man page: 
+    # --error-exitcode=1 makes it so taht any leaks cause a failure exit on the testcase
 	valgrind --leak-check=full --show-leak-kinds=all --error-exitcode=1 ./testcase "tc$i" 2>> "./valgrind_output.log"
 	ec=$?
 	test_result "$ec" "tc$i (Valgrind)"
@@ -45,21 +48,21 @@ done
 # ---------------------------------------------- 
 
 
-# Process reaping
+# Process reaping outline
 #     1. Write a test in testharness.sh that starts the srever in the background and captures the parent PID
-#     2. Send multiple client requests (decided on 50) triggering a fork() in the server and the child handles the client 
+#     2. Send multiple client requests triggering a fork() in the server and the child handles the client 
 #         and then the child has to exit. 
 #     3. Check /proc for zombies - locate children of parent PID and if there are ANY zombies (ANY!!) return FAIL
 #     4. Clean server shutdown. ^C should be a clean exit with no lingering children. 
 #     5. Run the reaping test under back to back
 #     6. Run the reaping test under heavy load (many requests)
-#     7. Run the reaping under slower or faster request arrival times (is this possible or necessary?)
 #
-#     NB      Entire test suite has to run under Valgrind automatically. 
+#           Entire test suite has to run under Valgrind automatically. 
 
 # (standard according to assignment - by default client.py 
 # is going to use localhost:8080 .)
 # Start server using valgrind 
+# valgrind --trace-children=yes 
 valgrind --leak-check=full  --show-leak-kinds=all --errors-for-leak-kinds=all --trace-children=yes ./lab01 "8080" > server.log   2>&1 &
 
 # capture server PID
@@ -69,9 +72,9 @@ SERVER_PID=$!
 sleep 1
 
 # SANITY CHECK: did the server crash? 
-if ! kill -0 $SERVER_PID 2>/dev/null; then
-    echo "SERVER CRASHED: Check server.log for errors"
-    exit 1
+# see line 109-110 - taking a different approach here
+if ps -ef | grep "$SERVER_PID" ; then
+    echo "Server is running"
 fi
 
 # SANITY CHECK: where's the server listening (netstat)?
@@ -106,25 +109,9 @@ fi
 echo "Waiting 15s for clients to finish network calls."
 sleep 15
 
-# Now count the zombies using /proc
-ZOMBIE_COUNT=0
-for pid in /proc/[0-9]*; do
-    if [ -f "$pid/status" ]; then
-    # proc output looks like this 
-    # State:  S (sleeping)
-    # ...
-    # PPid:   1 
-    # so awk '{print $2}' is just to get that second column out.
-        PARENT=$(grep "PPid:" "$pid/status" | awk '{print $2}')
-        STATE=$(grep "State:" "$pid/status" | awk '{print $2}')
-        
-        if [ "$PARENT" == "$SERVER_PID" ] && [ "$STATE" == "Z" ]; then
-            ((ZOMBIE_COUNT++))
-			# announce which process is a zombie - makes for easier debugging
-            echo "ZOMBIE! $pid"
-        fi
-    fi
-done
+# wait I totally dropped the ball on the old version of this - didn't always work
+# Redirecting a ps-ef works way better and might be better if /proc isn't synced at this point
+ZOMBIE_COUNT=$(ps -ef | grep $SERVER_PID | grep "<defunct" | wc -l)
 
 if [ "$ZOMBIE_COUNT" -gt 0 ]; 
 then
